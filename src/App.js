d15d57e83cd456ebf0db086f9b394be84b547dde
GageVanK <gagevank@gmail.com>
2023-01-03 12:56:28 -0500

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Profile from './pages/Profile'
+import Wallet from './pages/Wallet'
+import Notifications from './pages/Notifications'
+import Discover from './pages/Discover'
+import Home from './pages/Home'
+import Messages from './pages/Messages'
 
 function App() {
   return (
-    <h1>hello</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/messages" element={ <Messages /> } />
+        <Route path="/notifications" element={ <Notifications /> } />
+        <Route path="/discover" element={ <Discover /> } />
+        <Route path="/wallet" element={ <Wallet/> } />
+        <Route path="/" element={ <Home /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
