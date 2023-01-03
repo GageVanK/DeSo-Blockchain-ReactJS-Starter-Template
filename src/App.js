ea176eec561f926f82847e8e14488d68f452ed02
GageVanK <gagevank@gmail.com>
2023-01-03 15:09:39 -0500

    wrapping shell around page content

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Profile from './pages/Profile'
-import Wallet from './pages/Wallet'
-import Notifications from './pages/Notifications'
-import Discover from './pages/Discover'
-import Home from './pages/Home'
-import Messages from './pages/Messages'
+
 import MantineShell from './components/MantineShell'
 import { useState } from 'react';
 import { MantineProvider, ColorSchemeProvider} from '@mantine/core';
 
 function App() {
-  
   const [colorScheme, setColorScheme] = useState('light');
   const toggleColorScheme = (value) => {
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   }
 
   return (
-<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-<MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+  <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+  <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
     <MantineShell>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/profile" element={ <Profile /> } />
-        <Route path="/messages" element={ <Messages /> } />
-        <Route path="/notifications" element={ <Notifications /> } />
-        <Route path="/discover" element={ <Discover /> } />
-        <Route path="/wallet" element={ <Wallet/> } />
-        <Route path="/" element={ <Home /> } />
-      </Routes>
-    </BrowserRouter>
     </MantineShell>
     </MantineProvider>
     </ColorSchemeProvider>
--- src/components/MantineShell.js
+++ src/components/MantineShell.js
@@ -1,6 +1,13 @@
 //Using Matine UI to build out frontend.
 //Docs: https://mantine.dev/core/app-shell/
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Profile from '../pages/Profile'
+import Wallet from '../pages/Wallet'
+import Notifications from '../pages/Notifications'
+import Discover from '../pages/Discover'
+import Home from '../pages/Home'
+import Messages from '../pages/Messages'
 import {
   AppShell,
   Navbar,
@@ -73,7 +80,16 @@ export default function MantineShell() {
         </Header>
       }
     >
-      <Text>Resize app to see responsive navbar in action</Text>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/messages" element={ <Messages /> } />
+        <Route path="/notifications" element={ <Notifications /> } />
+        <Route path="/discover" element={ <Discover /> } />
+        <Route path="/wallet" element={ <Wallet/> } />
+        <Route path="/" element={ <Home /> } />
+      </Routes>
+    </BrowserRouter>
     </AppShell>
   );
 }
