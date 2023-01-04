b2f80f2821dadd13d612adf41dcf237cc4135d23
GageVanK <gagevank@gmail.com>
2023-01-03 22:11:16 -0500

    Adding NavLinks + Routing. Changed Moon icon.

--- src/components/MantineShell.js
+++ src/components/MantineShell.js
@@ -18,12 +18,112 @@ import {
   MediaQuery,
   Burger,
   useMantineTheme,
+  createStyles
 } from '@mantine/core';
+import {
+  IconBellRinging,
+  IconSettings,
+  IconReceipt2,
+  IconHome2,
+  IconUser,
+  IconDeviceDesktopAnalytics,
+  IconMessages,
+} from '@tabler/icons';
 import ThemeButton from '../components/ThemeButton'
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef('icon');
+  return {
+    header: {
+      paddingBottom: theme.spacing.md,
+      marginBottom: theme.spacing.md * 1.5,
+      borderBottom: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+      }`,
+    },
+
+    footer: {
+      paddingTop: theme.spacing.md,
+      marginTop: theme.spacing.md,
+      borderTop: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+      }`,
+    },
+
+    link: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
+    },
+
+    linkIcon: {
+      ref: icon,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+      marginRight: theme.spacing.sm,
+    },
+
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+          .background,
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+        [`& .${icon}`]: {
+          color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+        },
+      },
+    },
+  };
+});
+
+
+const data = [
+  { link: '/', label: 'Home', icon: IconHome2 },
+  { link: '/profile', label: 'Profile', icon: IconUser },
+  { link: '/discover', label: 'Discover', icon: IconDeviceDesktopAnalytics },
+  { link: '/messages', label: 'Messages', icon: IconMessages },
+  { link: '/notifications', label: 'Notifications', icon: IconBellRinging },
+  { link: '/wallet', label: 'Wallet', icon: IconReceipt2 },
+  { link: '/', label: 'Settings', icon: IconSettings },
+];
 
 export default function MantineShell() {
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
+  const { classes, cx } = useStyles();
+  const [active, setActive] = useState('Billing');
+
+  const links = data.map((item) => (
+    <a
+      className={cx(classes.link, { [classes.linkActive]: item.label === active })}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+
+        //This was is my way of bypassing react-router-dom Link to navigate between pages
+        window.location.href = item.link;
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
   return (
     <AppShell
       styles={{
@@ -33,10 +133,16 @@ export default function MantineShell() {
       }}
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
+
       navbar={
         <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+          <Navbar.Section>
           <ThemeButton/>
-        
+          </Navbar.Section>
+         
+          <Navbar.Section grow mt="xl">
+          {links}
+          </Navbar.Section>
         </Navbar>
       }
       aside={
@@ -63,18 +169,9 @@ export default function MantineShell() {
                 mr="xl"
               />
             </MediaQuery>
+            
 
-            <Text sx={{
-        fontSize: 18,
-
-        '&:hover': {
-          backgroundColor: '#eee',
-        },
-
-        '@media (max-width: 755px)': {
-          fontSize: 14,
-        },
-      }}>DeSo ReactJS Social App Template</Text>
+            <Text sx={{ fontSize: 18, fontWeight: 'bold'}}>DeSo ReactJS Social App Template</Text>
         
           </div>
         </Header>
